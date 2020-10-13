f8c2ddbab0a835dca49a1a275063ea29a1dc3a32
Fayçal Jebali <faycal.jebali1@gmail.com>
2020-10-13 23:06:02 +0200

    nettoyage de code

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -1,24 +1,17 @@
+const users = require('../controllers/user.controller.js');
 module.exports = (app) => {
-    const users = require('../controllers/user.controller.js');
-
     // Create a new User
     app.post('/api/users', users.create);
-
     // Retrieve all Users
     app.get('/api/users', users.findAll);
-
     // Retrieve a single User with userId
     app.get('/api/users/:id', users.findOne);
-
     // Attach Training to User
     app.put('/api/attach', users.attachTraining);
-
     // Retrieve a single User with userId
     app.get('/api/currentUser/:id', users.getCurrentUser);
-
     // Update a User with userId
     app.put('/api/users/:id', users.update);
-
     // Delete a User with userId
     app.delete('/api/users/:id', users.delete);
 }
