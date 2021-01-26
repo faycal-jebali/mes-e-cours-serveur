77299fea2629ed7666b4b13cea33b00f63980eac
Fayçal Jebali <faycal.jebali1@gmail.com>
2021-01-26 21:56:36 +0100

    stabiliser le back + change database to mongodb Atlas

--- config/database.config.js
+++ config/database.config.js
@@ -1,4 +1,5 @@
 module.exports = {
-    // url: 'mongodb://localhost:27017/easy-notes'
-    url: 'mongodb://souladaUser:Sd01234560@ds119060.mlab.com:19060/coursenligne'
-}
+  // url: 'mongodb://localhost:27017/easy-notes'
+  url:
+    "mongodb+srv://souladapro:wCbTeuoCKxbzzWCE@cluster0.a1wgl.mongodb.net/coursenligne?retryWrites=true&w=majority",
+};
