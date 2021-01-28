247687a9c0b8ebc89f315a6ad28a965f49d84cad
Fayçal Jebali <faycal.jebali1@gmail.com>
2021-01-28 21:25:20 +0100

    replace formation to course

---
+++ app/routes/course.routes.js
@@ -0,0 +1,36 @@
+module.exports = (app) => {
+  const courses = require("../controllers/course.controller.js");
+  const multer = require("multer");
+
+  const DIR = "./uploads/courses";
+
+  let storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, DIR);
+    },
+    filename: (req, file, cb) => {
+      console.log("req :: ", req.alias);
+      cb(null, file.fieldname + "-" + Date.now() + "." + file.originalname);
+      // cb(null, Date.now() + '.mp4');
+    },
+  });
+  let upload = multer({ storage: storage });
+
+  // Create a new Course
+  app.post("/api/courses", courses.create);
+
+  // Retrieve all courses
+  app.get("/api/courses", courses.findAll);
+
+  // Retrieve a single Course with id
+  app.get("/api/courses/:id", courses.findOne);
+
+  // Update a Course with id
+  app.put("/api/courses/:id", courses.update);
+
+  // Delete a Course with id
+  app.delete("/api/courses/:id", courses.delete);
+
+  // Upload
+  // app.post('/api/courses/upload', upload.single('uploaded'), courses.upload);
+};
